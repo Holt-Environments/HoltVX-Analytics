7564870bf931ba77461b6b02424ecb036d1c04f0
anthony mesa <anthony.mesa.prospt@gmail.com>
2021-01-14 16:06:21 -0500

    Created login and register capabilities using Identity service and connected to Azure cloud database.

--- Areas/Identity/Data/ApplicationUser.cs
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,12 @@ namespace HoltVXAnalytics.Areas.Identity.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string first_name { get; set; }
+        
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string last_name { get; set; }
     }
 }
